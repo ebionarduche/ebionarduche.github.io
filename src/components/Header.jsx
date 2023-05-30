438ccf1703048e9f46682cdc73ec83e8fc34f348
Ebio Narduche <ebionarduche77@gmail.com>
2023-05-30 16:56:57 -0300

    edição

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import './Header.css';
+import Curriculum from './icons/Curriculum';
+import GitHubSVG from './icons/GitHub';
+import LinkedinSVG from './icons/Linkedin';
 
 class Header extends Component {
     render() {
         return(
             <div className='Header'>
+                <div className='header-social-icons'>
+                    <LinkedinSVG />
+                    <GitHubSVG />
+                    <Curriculum />
+                </div>
                 <h1>REACT DEVELOPER</h1>
             </div>
         );
