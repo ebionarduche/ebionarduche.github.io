259ba2e750423dc89f129a9a4521c0fb14b86f94
Ebio Narduche <ebionarduche77@gmail.com>
2023-07-24 16:42:49 -0300

    Reformulando port

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,26 @@ import React from 'react';
 import './Header.css';
 import GitHubSVG from './icons/GitHub';
 import LinkedinSVG from './icons/Linkedin';
-// import fotoPerfil from '../fotoPerfil.png';
+import fotoPerfil from '../fotoPerfil.png';
 import Curriculum from './icons/Curriculum';
 
 
 
 const Header = () => {
-  
     return (
         <header>
-            <div className='first-title'>
-                <h1>Full Stack</h1>
-                <div className='header-social-icons'>
-                    <GitHubSVG />
-                    <LinkedinSVG />
-                    <Curriculum />
+            <section className='title'>
+                <div className='first-title'>
+                    <h1>Full Stack</h1>
+                    <div className='header-social-icons'>
+                        <GitHubSVG />
+                        <LinkedinSVG />
+                        <Curriculum />
+                    </div>
                 </div>
-            </div>
+                <h1>developer</h1>
+            </section>
+            <img className='perfil-img' src={fotoPerfil} alt="" />
         </header>
     );
 };
