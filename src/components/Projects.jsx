4e0b3a20dd9bea6327d0873274a43d8b6b46462b
Ebio Narduche <ebionarduche77@gmail.com>
2023-10-18 16:28:15 -0300

    Correções de Cores e Troca de Background

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react';
 import Card from './Card';
 import './style/Projects.css';
 import './style/Transition.css';
-import imagem from './images-projects/imagem-recipes-app.png';
+import projectRecipesApp from './images-projects/imagem-recipes-app.png';
+import projectPort from './images-projects/image-port.png';
 
 function Projects() {
     useEffect(() => {
@@ -31,8 +32,9 @@ function Projects() {
     return (
         <div id='projects' className="project-container">
             <h1>PROJETOS</h1>
+            <p>Aqui você encontrará alguns dos projetos pessoais <br /> que desenvolvi durante meus estudos em programação.</p>
             <div className="individual-project init-hidden">
-                <img src={imagem} alt="imagem" className="project-img"/>
+                <img src={projectRecipesApp} alt="imagem" className="project-img mobile-img"/>
                 <Card 
                     projectName='Recipes App' 
                     subTitle='Front-end'
@@ -43,27 +45,27 @@ function Projects() {
                     Plink={liveRecipesApp}     
                 />
             </div>
-
             <div className="individual-project init-hidden">
-                <img src={imagem} alt="imagem" className="project-img"/>
+                <img src={projectPort} alt="imagem" className="project-img desktop-img"/>
                 <Card 
                     projectName='Table Futebol Club' 
                     subTitle='Back-end'
                     description='O TFC é um site informativo sobre partidas e classificações de futebol.
                     Este projeto inclui uma API desenvolvida em Node.js que permite criar, editar,
                     excluir e selecionar partidas.'
-                    skills={['TypeScript', 'NodeJs', 'POO', 'Sequelize', 'MySql','RTL', 'Sinon', 'Mocka', 'Docker', 'JWT' ]}
+                    skills={['TypeScript', 'NodeJs', 'POO', 'Sequelize', 'MySql', 'Sinon', 'Mocka', 'Docker', 'JWT' ]}
                     Rlink={githubLink}     
                 />
             </div>
 
             <div className="individual-project init-hidden">
-                <img src={imagem} alt="imagem" className="project-img"/>
+                <img src={projectPort} alt="imagem" className="project-img desktop-img"/>
                 <Card 
-                    projectName='Recipes App' 
+                    projectName='Portfolio' 
+                    subTitle='Front-end'
                     description='Receitas App é um aplicativo mobile que permite acessar uma ampla variedade de receitas,
                     refeições e drinks. Com ele, você pode pesquisar por nome de alimentos e nome de receitas.'
-                    skills={['JavaScript', 'React', 'CSS', 'BrowserRouter', 'ContextAPI','RTL', 'Redux', 'Bootstrap' ]}
+                    skills={['JavaScript', 'React', 'CSS', 'ContextAPI' ]}
                     Rlink={githubLink}     
                 />
             </div>
